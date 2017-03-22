0a777d034049e1839ca856ca6c9fb964303ad8bc
prosbloom225 <prosbloom@gmail.com>
2017-03-22 17:44:49 -0500

    init iitem

---
+++ src/IItem.h
@@ -0,0 +1,23 @@
+#ifndef IITEM_H 
+#define IITEM_H
+
+
+#include <string>
+
+class IItem {
+    private:
+        int itemId;
+        std::string *name;
+
+
+    public:
+        virtual ~IInventory() = default;
+
+
+        // getters
+        virtual std::string *get_name() { return name; };
+        virtual int get_itemId() { return itemId; };
+};
+
+
+#endif
